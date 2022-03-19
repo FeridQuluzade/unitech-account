985e1715c4d5adb6cb31eaef306eced9dfbf09d6
FeridQuluzade <feridquluzade2002@gmail.com>
2022-03-20 00:22:36 +0400

    initial commit

---
+++ src/main/java/az/unitech/development/account/MsAccountApplication.java
@@ -0,0 +1,13 @@
+package az.unitech.development.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/unitech/development/account/MsAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package az.unitech.development.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
